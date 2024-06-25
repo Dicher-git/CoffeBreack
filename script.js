018129e97fe993176d2d0d7df3fb9ce4575a45e8
Dima <dechir07@gmail.com>
2024-06-25 09:40:09 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+  document.body.classList.add('loaded');
+  document.body.classList.remove('loaded_hiding');
+  }, 8600);
+}
+
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav_bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav_bar";
+    }
+    
+    }
